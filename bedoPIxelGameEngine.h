7ac9fbf0dad0eb70a4a552b97fbd115f020fb120
Bedoshady <54337845+Bedoshady@users.noreply.github.com>
2021-08-12 14:43:33 +0200

    Add files via upload

--- bedoPIxelGameEngine.h
+++ bedoPIxelGameEngine.h
@@ -12,7 +12,7 @@
 class Renderer;
 
 class bedoPixelGameEngine {
-
+public:
 	struct pixel {
 		union rgb {
 			uint32_t c;
@@ -28,20 +28,20 @@ class bedoPixelGameEngine {
 
 
 		}
-		float GetRed() {
-			return (float)color.b[3] / 255.0f;
+		int GetRed() {
+			return (float)color.b[0] ;
 		}
-		float GetGreen() {
-			return (float)color.b[2] / 255.0f;
+		int GetGreen() {
+			return (float)color.b[1] ;
 		}
-		float GetBlue() {
-			return (float)color.b[1] / 255.0f;
+		int GetBlue() {
+			return (float)color.b[2] ;
 		}
 
 	};
 
-	bool OnUserCreate() { return true; };
-	bool OnUserUpdate(float fElapseTime) { return true; };
+	virtual bool OnUserCreate() { return true; };
+	virtual bool OnUserUpdate(float fElapseTime) { return true; };
 
 private:
 	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
@@ -56,58 +56,189 @@ private:
 			break;
 		}
 	}
-	int positionx = 0;
-	int positiony = 0;
-#define GL_UNSIGNED_INT_8_8_8_8_REV 0x8367
-#define GL_UNSIGNED_INT_8_8_8_8 0x8035
-public: 
 
+public: 
+	void Genertaetexture() {
+		uint32_t id;
+		glGenTextures(1, &id);
+		glBindTexture(GL_TEXTURE_2D, id);
+		glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+		glEnable(GL_TEXTURE_2D);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+			GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+			GL_LINEAR);
+
+	}
 	void display() {
 		
 		glViewport(0, 0, m_width, m_height);
 		 
 		glClear(GL_COLOR_BUFFER_BIT);
-	
-		for (int i = 0; i < m_height; i++) {
-			for (int j = 0; j < m_width; j++) {
-			//	DrawPixel(j, i, 255,  255, 255);
-				
-				//pixel h;
-				//h.SetRGB(255,0,255);
-				
-			//	 m_screenBuffer[i * m_width + j] = h;
-			}
-		}
+
+		//glPixelStorei(GL_PACK_ROW_LENGTH, m_width);
+
+//		glMatrixMode(GL_PROJECTION);
+		//glLoadIdentity();
+		//gluOrtho2D(-1,m_width,m_height, -1);
+		//gluOrtho2D()
+		//glMatrixMode(GL_MODELVIEW);
+
+		for (int i = 0; i < m_width * m_height; i++)
+			buffer[i] = m_screenBuffer[i].color.c;
+
 		
-		//glDrawPixels(m_width, m_height,GL_RGBA, GL_UNSIGNED_BYTE, m_screenBuffer);
+		
+		int toptexel = 1, lefttexel = -1, righttexel = 1, bottomtexel = -1;
+		
+		glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,m_width, m_height,0 ,GL_RGBA, GL_UNSIGNED_BYTE, buffer);
+		//glTexSubImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_width, m_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
+		
+		glBegin(GL_QUADS);
 
-		//glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,m_width, m_height,0 ,GL_RGBA, GL_UNSIGNED_BYTE, m_screenBuffer);
+		glTexCoord2f(1, 1); glVertex2f((GLfloat)righttexel, (GLfloat)bottomtexel);
+		glTexCoord2f(0, 1); glVertex2f((GLfloat)lefttexel, (GLfloat)bottomtexel);
+		glTexCoord2f(0, 0); glVertex2f((GLfloat)lefttexel, (GLfloat)toptexel);
+		glTexCoord2f(1, 0); glVertex2f((GLfloat)righttexel, (GLfloat)toptexel);
 
-
+		glEnd();
+		//glDrawPixels(m_width, m_height,GL_RGBA, GL_UNSIGNED_BYTE, buffer);
 		SwapBuffers(hdc);
 
 	}
 public:
-	void DrawPixel(int x, int y, int r, int g, int b) {
-		pixel rgb;
-		rgb.SetRGB(r, g, b);
+
+	void DrawLine(int x1, int y1, int x2, int y2, pixel rgb) {
+		auto swap = [&](int x, int y) {int acc = x; x = y; y = acc; };
+			if (x2 > x1) {
+				swap(x2, x1);
+				swap(y2, y1);
+			}
+
+			if (x2 - x1 == 0)
+			{
+				if (y1 > y2)
+					swap(y1, y2);
+				for (int i = y1; i <= y2; i++)
+					DrawPixel(x1, i, rgb.GetRed(), rgb.GetGreen(), rgb.GetBlue());
 			
-		if (x >= 0 && x < m_width && y >= 0 && y <  m_height)
-					m_screenBuffer[(y) * m_width + x ] = rgb;
+			
+				return;
+			}
+			if (y2 - y1 == 0) {
+				for (int i = x1; i <= x2; i++)
+					DrawPixel(i, y1, rgb.GetRed(), rgb.GetGreen(), rgb.GetBlue());
+				return;
+			}
+			if (abs(x2 - x1) > abs(y2 - y1))
+			{
+				int sign = 0;
+				if (y2 - y1 > 0)
+					sign = 1;
+				else
+					sign = -1;
+
+
+				int dx = abs(x2 - x1);
+				int dy = abs(y2 - y1);
+				int acc = 0;
+				int y = y1;
+				for (int i = x1; i <= x2; i++) {
+					DrawPixel(i, y, rgb.GetRed(), rgb.GetGreen(), rgb.GetBlue());
+					acc += 2 * dy;
+					if (2 * acc > dx) {
+						y += sign;
+						acc -= 2 * dx;
+
+					}
+
+
+				}
+
+
+			}
+			else {
+
+				if (y1 > y2)
+				{
+					swap(y1, y2);
+					swap(x1, x2);
+				}
+
+
+				int sign = 0;
+				if (x2 - x1 > 0)
+					sign = 1;
+				else
+					sign = -1;
+
+
+				int dx = abs(x2 - x1);
+				int dy = abs(y2 - y1);
+				int acc = 0;
+				int x = x1;
+				for (int i = y1; i <= y2; i++) {
+					DrawPixel(x, i, rgb.GetRed(), rgb.GetGreen(), rgb.GetBlue());
+					acc += 2 * dx;
+					if (2 * acc > dy) {
+						x += sign;
+						acc -= 2 * dy;
+
+					}
+
+
+				}
+
+
+
+			}
 
 
 
 	}
+
+
+
+
+
+
+
+
+	void DrawPixel(int x, int y, int r, int g, int b) {
+		pixel rgb;
+		rgb.SetRGB(r, g, b);
+			for(int i  = 0; i < m_fontY; i++)
+				for (int j = 0; j < m_fontX; j++) {
+					int posx = m_fontX * x + j;
+					int posy = m_fontY * y + i;
+					if (posx >= 0 && posx < m_width && posy >= 0 && posy < m_height)
+						m_screenBuffer[(posy)*m_width + posx] = rgb;
+				}
+
+
+	}
 	void ConstructPixel(int width, int height, int posx, int posy, int fontX, int fontY, std::wstring name) {
-		m_width = width ;
-		m_height = height;
+		m_width = width * fontX;
+		m_height = height * fontY;
 		m_fontX = fontX;
 		m_fontY = fontY;
 		wc.lpfnWndProc =  WindowProc;
+		//gsgfsfs
 		wc.hInstance = GetModuleHandle(nullptr);
 		wc.hbrBackground = (HBRUSH)COLOR_BACKGROUND;
 		wc.lpszClassName = name.c_str();
 		RegisterClass(&wc);
+
+		DWORD dwExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;
+		DWORD dwStyle = WS_CAPTION | WS_SYSMENU | WS_VISIBLE | WS_THICKFRAME;
+		RECT rWndRect = { 0, 0, m_width, m_height };
+		AdjustWindowRectEx(&rWndRect, dwStyle, FALSE, dwExStyle);
+		int mwidth = rWndRect.right - rWndRect.left;
+		int mheight = rWndRect.bottom - rWndRect.top;
+
+
 		//buffer = new uint32_t[m_width * m_height];
 
 		m_hwnd = CreateWindowEx(
@@ -117,7 +248,7 @@ public:
 			WS_OVERLAPPEDWINDOW,            // Window style
 
 			// Size and position
-			posx, posy, m_width, m_height,
+			posx, posy, mwidth, mheight  ,
 
 			NULL,       // Parent window    
 			NULL,       // Menu
@@ -127,8 +258,8 @@ public:
 		//enableOpengl(m_hwnd);
 
 
-		m_screenBuffer = new pixel[m_width * m_height];
-		memset(m_screenBuffer, 0, sizeof(pixel) * m_width * m_height);
+		m_screenBuffer = new pixel[(m_width) * (m_height)];
+		memset(m_screenBuffer, 0, sizeof(pixel) * (m_width) * (m_height));
 	}
 	void enableOpengl(const HWND& hwnd) {
 		int pf = 0;
@@ -173,7 +304,7 @@ public:
 		ShowWindow(hwnd, 1);
 	}
 
-
+	 int32_t *buffer;
 	void viewDisplay() {
 		enableOpengl(m_hwnd);
 		auto t1 = std::chrono::high_resolution_clock::now();
@@ -181,17 +312,18 @@ public:
 
 		std::chrono::duration<float> elapseTime = t2 - t1;
 		float fElapseTime = elapseTime.count();
-		
-		
+		Genertaetexture();
+		OnUserCreate();
+		buffer = new int32_t[m_width * m_height];
 		while (1) {
 		//	display();
 			t2 = std::chrono::high_resolution_clock::now();
 			elapseTime = t2 - t1;
 			fElapseTime = elapseTime.count();
 			t1 = t2;
+			OnUserUpdate(fElapseTime);
 			display();
 			//std::cout << 1.0f / fElapseTime << std::endl;;
-			//OnUserUpdate(fElapseTime);
 			std::wstring h = L"bedoPixelgameEngine " + std::to_wstring(1.0f / fElapseTime);
 			SetWindowText(m_hwnd, h.c_str());
 		
--- main.cpp
+++ main.cpp
@@ -5,9 +5,52 @@
 
 
 #include "bedoPIxelGameEngine.h"
+
+class example : public bedoPixelGameEngine
+{
+    bool OnUserCreate() {
+
+        return true;
+    }
+public:
+    int width = 256 * 4, height = 176 * 4;
+        float x=0, y=0;
+    bool OnUserUpdate(float fElpasedTime) {
+
+//        DrawPixel(x, y, 128, 0, 0);
+        for (int i = 0; i < height ; i++)
+            for (int j = 0; j < width; j++)
+                if (i > 80)
+                    DrawPixel(j, i, 0, 128, 0);
+                else
+                    DrawPixel(j, i, 0, 0, 128);
+        
+        if (x >= width)
+                x = 0;
+                //y = 2;
+        int speed = 20;
+        x += (float)speed * fElpasedTime;
+        
+                //for(int i  =0; i < 20; i++)
+                //for(int j = 0; j < 20; j++)
+     
+                 //   DrawPixel(x + j, y + i, 128, 0, 0);
+                //pixel rgb;
+                //rgb.SetRGB(128, 0, 128);
+
+               // DrawLine(0, 0, 6, 10, rgb);
+
+           
+        return true;
+    }
+
+};
+
+
 int main() {
-    bedoPixelGameEngine j;
-    j.ConstructPixel(640, 320, 0, 0, 4, 4, L"hello");
+    example j;
+    
+    j.ConstructPixel(j.width, j.height, 0, 0, 1, 1, L"hello");
     j.run();
 	return 0;
 }
